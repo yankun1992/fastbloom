a03a2e95e6d9b9347ee4e8dd5b65708da04a9920
yankun <1939810907@qq.com>
2022-05-23 22:15:41 +0800

    use env to choose conda env for develop.

--- build.py
+++ build.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+# build script for local develop.
+
 import os
 
-_TEST_CONDA_ENV = "py37"
+_TEST_CONDA_ENV = os.environ['BLOOM_CONDA_ENV'] if 'BLOOM_CONDA_ENV' in os.environ else 'py37'
 
 
 def develop():
