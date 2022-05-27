eb61fa2af4211d74131973ee936210db4c012661
yankun <1939810907@qq.com>
2022-05-28 00:13:53 +0800

    rust benchmark

--- benches/fastbloom.rs
+++ benches/fastbloom.rs
@@ -31,13 +31,13 @@ fn bloom_hash<T>(hashes: &mut [u64; 2], item: &T, k_i: u32, sips: &mut [SipHashe
 }
 
 
-fn filter_test(filter: &mut BloomFilter, inputs: &[String]) {
+fn bloom_add_all_test(filter: &mut BloomFilter, inputs: &[String]) {
     for input in inputs {
         filter.add(input.as_bytes());
     }
 }
 
-fn filter_add_test(filter: &mut BloomFilter, data: &[u8]) {
+fn bloom_add_test(filter: &mut BloomFilter, data: &[u8]) {
     filter.add(data);
 }
 
@@ -45,50 +45,53 @@ fn as_test(m: u128) -> u64 {
     m as u64
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let items_count = 100_000_000;
+fn mod_bench(c: &mut Criterion) {
+    c.bench_function("mod_u128", |b| b.iter(|| black_box(43567890u128) % black_box(1024u128)));
+    c.bench_function("mod_u64", |b| b.iter(|| black_box(43567890u64) % black_box(1024u64)));
+    c.bench_function("add_u64", |b| b.iter(|| black_box(43567890u64) + black_box(1024u64)));
+}
+
+fn hash_bench(c: &mut Criterion) {
+    let hello = "hellohellohellohello".to_string();
+    let mut crc = CRCHasher::new();
+    c.bench_function("crc32fast", |b| b.iter(|| {
+        crc.update(black_box(hello.as_bytes()));
+        crc.finish();
+    }));
+
     let mut seed = [0u8; 32];
-
     getrandom(&mut seed).unwrap();
-    let inputs: Vec<String> = (1..1_000_000).map(|n| { n.to_string() }).collect();
-
-    let mut hashes = [0u64; 2];
-
     let mut k1 = [0u8; 16];
     let mut k2 = [0u8; 16];
     k1.copy_from_slice(&seed[0..16]);
     k2.copy_from_slice(&seed[16..32]);
     let mut sips: [SipHasher13; 2] = [sip_new(&k1), sip_new(&k2)];
+    let mut hashes = [0u64; 2];
+    c.bench_function("bloom_hash", |b| b.iter(|| bloom_hash(&mut hashes,
+                                                            &black_box("hellohellohellohello"), black_box(1), &mut sips)));
+    c.bench_function("fastmurmur3", |b| b.iter(|| murmur3_x64_128(black_box(b"hellohellohellohello"), 0)));
+
+    let mut fxhash = FxHasher64::default();
+    c.bench_function("fxhash", |b| b.iter(|| {
+        hello.hash(&mut fxhash);
+        fxhash.finish();
+    }));
+}
+
+fn bloom_add_bench(c: &mut Criterion) {
+    let inputs: Vec<String> = (1..1_000_000).map(|n| { n.to_string() }).collect();
+    let items_count = 100_000_000;
 
     let hello = "hellohellohellohello".to_string();
 
-    let mut filter = FilterBuilder::new(items_count as u64, 0.01)
-        .build_bloom_filter();
+    let mut filter = FilterBuilder::new(items_count as u64, 0.001).build_bloom_filter();
 
-    let mut fxhash = FxHasher64::default();
+    c.bench_function("bloom_add_test", |b| b.iter(|| bloom_add_test(&mut filter, black_box("hellohellohellohello".as_bytes()))));
+    c.bench_function("bloom_add_all_test", |b| b.iter(|| bloom_add_all_test(&mut filter, &inputs[..])));
 
-    let mut crc = CRCHasher::new();
-
-
-    // c.bench_function("mod_u128", |b| b.iter(|| black_box(43567890u128) % black_box(1024u128)));
-    // c.bench_function("mod_u64", |b| b.iter(|| black_box(43567890u64) % black_box(1024u64)));
-    // c.bench_function("add_u64", |b| b.iter(|| black_box(43567890u64) + black_box(1024u64)));
-    // c.bench_function("crc32fast", |b| b.iter(|| {
-    //     crc.update(black_box(hello.as_bytes()));
-    //     crc.finish();
-    // }));
-    // c.bench_function("bloom_hash", |b| b.iter(|| bloom_hash(&mut hashes, &black_box("hellohellohellohello"), black_box(1), &mut sips)));
-    c.bench_function("fastmurmur3", |b| b.iter(|| murmur3_x64_128(black_box(b"hellohellohellohello"), 0)));
-    c.bench_function("filter_add_test", |b| b.iter(|| filter_add_test(&mut filter, black_box("hellohellohellohello".as_bytes()))));
-    // c.bench_function("fxhash", |b| b.iter(|| {
-    //     hello.hash(&mut fxhash);
-    //     fxhash.finish();
-    // }));
-
-    // c.bench_function("as_test", |b| b.iter(|| as_test(black_box(1000000))));
-
-    c.bench_function("filter_test", |b| b.iter(|| filter_test(&mut filter, &inputs[..])));
+    c.bench_function("bloom_contains_test", |b| b.iter(|| filter.contains(black_box(hello.as_bytes()))));
+    c.bench_function("bloom_not_contains_test", |b| b.iter(|| filter.contains(black_box(b"hellohellohello"))));
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, bloom_add_bench, hash_bench);
 criterion_main!(benches);
